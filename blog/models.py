14ceba465a60db01448b812dd0c1753ebb11899a
JakeRatliff <wrjake@gmail.com>
2018-08-12 19:20:32 -0400

    added blog gufunctionality

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,9 @@ class Blog(models.Model):
 	title = models.CharField(max_length = 70)
 	pub_date = models.DateTimeField()
 	body = models.CharField(max_length = 20000)
+
+	def summary(self):
+		return self.body[:100]
+
+	def __str__(self):
+		return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -13,18 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
+from django.urls import path
 from . import views
-import jobs.views
-
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', jobs.views.home, name='home'),
-    path('dolly', views.home),
-    path('blogs/', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.allblogs, name='blogs/allblogs'),
+    path('<int:blog_id>', views.detail, name='blogs/detail')
+]
